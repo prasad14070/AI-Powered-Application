f10651b49464c6cc0648cce2e9de125bb59fa2ce
prasad <prasadbhujbal1407@gmail.com>
2025-03-21 15:52:56 +0530

    Added GitHub Action to automate commits

--- commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\Prasad Bhujbal\Downloads\AI-Powered-Application
-echo Automated commit on %DATE% %TIME% >> log.txt
-git add .
-git commit -m "Automated commit on %DATE% %TIME%"
-git push origin main
